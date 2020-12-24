6eb0d5b67d1ae6e1b209fe641fea3577baa8229d
Med-Process <57434913+Med-Process@users.noreply.github.com>
2020-12-24 16:35:16 +0800

    Create segmentation_review.py

---
+++ segmentation_review.py
@@ -0,0 +1,3 @@
+### Lung Segmentation
+1.(2020) A Comprehensive Review of Computer-Aided Diagnosis of Pulmonary Nodules Based on Computed Tomography Scans. https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9173767
+该论文对Pulmonary Nodules的CAD系统的发展和应用作了综述性回顾，包括任务、数据集、评价指标、最近的方法。
